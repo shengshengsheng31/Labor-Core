f6f7009431e565b70666fb8860a9dcad34f89af3
盛盛盛 <1445089856@qq.com>
2020-06-09 16:16:02 +0800

    MD5加密

---
+++ Labor.Common/Md5Helper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Labor.Common
+{
+    public class Md5Helper
+    {
+        /// <summary>
+        /// Md5加密
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string Md5Encrypt(string password)
+        {
+            //判断非空
+            if (string.IsNullOrEmpty(password) || string.IsNullOrWhiteSpace(password))
+            {
+                return string.Empty;
+            }
+
+            var pwd = string.Empty;
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(password);
+                byte[] newBuffer = md5.ComputeHash(buffer);
+                foreach (var item in newBuffer)
+                {
+                    pwd = string.Concat(pwd, item.ToString("X2"));
+                }
+            }
+            return pwd;
+        }
+    }
+}
